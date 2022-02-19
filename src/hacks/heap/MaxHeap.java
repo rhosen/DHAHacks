b22ef814900d5ad608d09c818378526fa5e136f5
Rubel Hosen <rhosen@epharmasolutions.com>
2022-02-20 04:18:09 +0600

    refactor

--- src/hacks/heap/MaxHeap.java
+++ src/hacks/heap/MaxHeap.java
@@ -1,4 +1,5 @@
 package hacks.heap;
+
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -9,16 +10,17 @@ public class MaxHeap {
     public static void main(String[] args) {
         var arr = new int[]{5, 3, 8, 2, 0};
         var k = 2;
-        var res = maxHeap(arr, k);
+        MaxHeap heap = new MaxHeap();
+        var res = heap.find(arr, k);
         System.out.println(res); // 3
     }
 
     // delete all except k small element from max heap
-    static int maxHeap(int[] array, int k) {
+    int find(int[] array, int k) {
         var pq = new PriorityQueue<Integer>(Comparator.reverseOrder());
-        for(int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             pq.add(array[i]);
-            if( pq.size() > k+1) pq.poll();
+            if (pq.size() > k + 1) pq.poll();
         }
         return pq.poll();
     }
