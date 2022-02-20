80856a80b1b3bebceeef9b4b2f45c341f7e6cc08
Rubel Hosen <rhosen@epharmasolutions.com>
2022-02-20 23:01:03 +0600

    fix heap sort

--- src/hacks/heap/HeapSort.java
+++ src/hacks/heap/HeapSort.java
@@ -6,64 +6,62 @@ import java.util.Arrays;
 
 public class HeapSort {
 
-    // solution doesn't work
-
     public static void main(String[] args) {
-        var arr = new int[]{64, 65, 65, 72, 77, 80, 85, 86, 86, 92, 91, 92, 93, 95, 98};
-        buildMaxHeap(arr);
-        sort(arr);
+        var arr = new int[]{5,2,0,3,1,4,8};
+        HeapSort hp = new HeapSort();
+        hp.sortArray(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void buildMaxHeap(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            siftUp(array, i);
+    void sortArray(int[] array) {
+        var n = array.length;
+        var middle = n/2;
+        for(int i = middle; i >= 0 ; i--){
+            shiftDown(array, i, n);
         }
+        sort(array);
     }
 
-    static void siftUp(int[] array, int childIndex) {
-        // if we are at pos 0 that means we reached the root and there is no other parent to compare with
-        while (childIndex > 0) {
-            var parentIndex = childIndex / 2;
-            if (array[parentIndex] < array[childIndex]) {
-                swap(array, childIndex, parentIndex);
-                childIndex = parentIndex; // update the current node
-            } else {
+    // build max heap
+    void shiftDown(int[] array, int root, int size) {
+        var left = getLeftChild(root);
+        var right = getRightChild(root);
+        while (left < size){
+            var maxChildIndex = left;
+            if(right < size && array[right] > array[left]){
+                maxChildIndex = right;
+            }
+            if(array[maxChildIndex] > array[root]){
+                swap(array, root, maxChildIndex);
+                root = maxChildIndex;
+                left = getLeftChild(root);
+                right = getRightChild(root);
+            }else{
                 break;
             }
         }
     }
 
-    private static void swap(int[] array, int i, int j) {
-        var temp = array[j];
-        array[j] = array[i];
-        array[i] = temp;
-    }
-
-    static void sort(int[] array) {
+    void sort(int[] array) {
         for (int i = array.length - 1; i > 0; i--) {
+            // swap the largest element to last index
             swap(array, i, 0);
+            // restore max heap
             shiftDown(array, 0, i);
         }
     }
 
-    static void shiftDown(int[] array, int maxElementIndex, int size) {
-        var left = maxElementIndex * 2 + 1;
-        var right = maxElementIndex * 2 + 2;
-        while (left < size) {
-            var maxChildIndex = left;
-            if (right < size && array[right] > array[left]) {
-                maxChildIndex = right;
-            }
-            if (array[maxElementIndex] < array[maxChildIndex]) {
-                swap(array, maxElementIndex, maxChildIndex);
-                maxElementIndex = maxChildIndex;
-                left = maxElementIndex * 2 + 1;
-                right = maxElementIndex * 2 + 2;
-            } else {
-                break;
-            }
-        }
+    int getLeftChild(int root){
+        return root*2+1;
     }
 
+    int getRightChild(int root){
+        return root*2+2;
+    }
+
+    void swap(int[] array, int i, int j) {
+        var temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
 }
